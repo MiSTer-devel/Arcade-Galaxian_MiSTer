f2371ba428215cd0fae5b58e8aec9f87a2c77a80
sorgelig <pour.garbage@gmail.com>
2018-03-13 17:26:37 +0800

    Update API.

--- Arcade-Galaxian.sv
+++ Arcade-Galaxian.sv
@@ -29,7 +29,7 @@ module emu
 	input         RESET,
 
 	//Must be passed to hps_io module
-	inout  [43:0] HPS_BUS,
+	inout  [44:0] HPS_BUS,
 
 	//Base video clock. Usually equals to CLK_SYS.
 	output        VGA_CLK,
@@ -57,7 +57,8 @@ module emu
 	output  [7:0] HDMI_B,
 	output        HDMI_HS,
 	output        HDMI_VS,
-	output        HDMI_DE,    // = ~(VBlank | HBlank)
+	output        HDMI_DE,   // = ~(VBlank | HBlank)
+	output  [1:0] HDMI_SL,   // scanlines fx
 
 	//Video aspect ratio for HDMI. Most retro systems have ratio 4:3.
 	output  [7:0] HDMI_ARX,
@@ -65,7 +66,7 @@ module emu
 
 	output        LED_USER,  // 1 - ON, 0 - OFF.
 
-	// b[1]: 0 - LED status is system status ORed with b[0]
+	// b[1]: 0 - LED status is system status OR'd with b[0]
 	//       1 - LED status is controled solely by b[0]
 	// hint: supply 2'b00 to let the system control the LED.
 	output  [1:0] LED_POWER,
@@ -73,45 +74,8 @@ module emu
 
 	output [15:0] AUDIO_L,
 	output [15:0] AUDIO_R,
-	output        AUDIO_S, // 1 - signed audio samples, 0 - unsigned
-	input         TAPE_IN,
-
-	// SD-SPI
-	output        SD_SCK,
-	output        SD_MOSI,
-	input         SD_MISO,
-	output        SD_CS,
-
-	//High latency DDR3 RAM interface
-	//Use for non-critical time purposes
-	output        DDRAM_CLK,
-	input         DDRAM_BUSY,
-	output  [7:0] DDRAM_BURSTCNT,
-	output [28:0] DDRAM_ADDR,
-	input  [63:0] DDRAM_DOUT,
-	input         DDRAM_DOUT_READY,
-	output        DDRAM_RD,
-	output [63:0] DDRAM_DIN,
-	output  [7:0] DDRAM_BE,
-	output        DDRAM_WE,
-
-	//SDRAM interface with lower latency
-	output        SDRAM_CLK,
-	output        SDRAM_CKE,
-	output [12:0] SDRAM_A,
-	output  [1:0] SDRAM_BA,
-	inout  [15:0] SDRAM_DQ,
-	output        SDRAM_DQML,
-	output        SDRAM_DQMH,
-	output        SDRAM_nCS,
-	output        SDRAM_nCAS,
-	output        SDRAM_nRAS,
-	output        SDRAM_nWE
+	output        AUDIO_S    // 1 - signed audio samples, 0 - unsigned
 );
-
-assign {DDRAM_CLK, DDRAM_BURSTCNT, DDRAM_ADDR, DDRAM_DIN, DDRAM_BE, DDRAM_RD, DDRAM_WE} = 0;
-assign {SD_SCK, SD_MOSI, SD_CS} = 'Z;
-assign {SDRAM_DQ, SDRAM_A, SDRAM_BA, SDRAM_CLK, SDRAM_CKE, SDRAM_DQML, SDRAM_DQMH, SDRAM_nWE, SDRAM_nCAS, SDRAM_nRAS, SDRAM_nCS} = 'Z;
 
 assign LED_USER  = ioctl_download;
 assign LED_DISK  = 0;
@@ -126,6 +90,7 @@ localparam CONF_STR = {
 	"-;",
 	"O1,Aspect Ratio,Original,Wide;",
 	"O2,Orientation,Vert,Horz;",
+	"O34,Scanlines(vert),No,25%,50%,75%;",
 	"-;",
 	"T6,Reset;",
 	"J,Fire,Start 1P,Start 2P;",
@@ -137,6 +102,7 @@ localparam CONF_STR = {
 wire clk_sys, clk_18, clk_6;
 wire pll_locked;
 
+wire clk_hdmi;
 pll pll
 (
 	.refclk(CLK_50M),
@@ -144,6 +110,7 @@ pll pll
 	.outclk_0(clk_18),
 	.outclk_1(clk_sys),
 	.outclk_2(clk_6),
+	.outclk_3(clk_hdmi),
 	.locked(pll_locked)
 );
 
@@ -157,7 +124,7 @@ wire        ioctl_wr;
 wire [24:0] ioctl_addr;
 wire  [7:0] ioctl_dout;
 
-wire [64:0] ps2_key;
+wire [10:0] ps2_key;
 
 wire [15:0] joystick_0,joystick_1;
 wire [15:0] joy = joystick_0 | joystick_1;
@@ -182,14 +149,13 @@ hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
 	.ps2_key(ps2_key)
 );
 
-wire pressed    = (ps2_key[15:8] != 8'hf0);
-wire extended   = (~pressed ? (ps2_key[23:16] == 8'he0) : (ps2_key[15:8] == 8'he0));
-wire [8:0] code = ps2_key[63:24] ? 9'd0 : {extended, ps2_key[7:0]}; // filter out PRNSCR and PAUSE
+wire       pressed = ps2_key[9];
+wire [8:0] code    = ps2_key[8:0];
 always @(posedge clk_sys) begin
 	reg old_state;
-	old_state <= ps2_key[64];
+	old_state <= ps2_key[10];
 	
-	if(old_state != ps2_key[64]) begin
+	if(old_state != ps2_key[10]) begin
 		casex(code)
 			'hX75: btn_up          <= pressed; // up
 			'hX72: btn_down        <= pressed; // down
@@ -239,7 +205,7 @@ assign VGA_DE   = ~(hblank | vblank);
 assign VGA_HS   = ~hs;
 assign VGA_VS   = ~vs;
 
-assign HDMI_CLK = VGA_CLK;
+assign HDMI_CLK = status[2] ? VGA_CLK: clk_hdmi;
 assign HDMI_CE  = status[2] ? VGA_CE : 1'b1;
 assign HDMI_R   = status[2] ? VGA_R  : {rr,rr,rr[2:1]};
 assign HDMI_G   = status[2] ? VGA_G  : {rg,rg,rg[2:1]};
@@ -247,6 +213,7 @@ assign HDMI_B   = status[2] ? VGA_B  : {rb,rb,rb[2:1]};
 assign HDMI_DE  = status[2] ? VGA_DE : rde;
 assign HDMI_HS  = status[2] ? VGA_HS : rhs;
 assign HDMI_VS  = status[2] ? VGA_VS : rvs;
+assign HDMI_SL  = status[2] ? 2'd0   : status[4:3];
 
 screen_rotate #(257,224,9) screen_rotate
 (
@@ -256,7 +223,7 @@ screen_rotate #(257,224,9) screen_rotate
 	.hblank(hblank),
 	.vblank(vblank),
 
-	.clk_out(clk_sys),
+	.clk_out(clk_hdmi),
 	.video_out({rr,rg,rb}),
 	.hsync(rhs),
 	.vsync(rvs),
